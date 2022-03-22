58296cb49cce092a3d9dbd965d9a13d164798586
GPancaldi <92167152+GPancaldi@users.noreply.github.com>
2022-03-22 18:48:01 -0300

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const testeRoutes = require('./teste');
-
-const apiRouter = express.Router();
-
-apiRouter.use('/teste', testeRoutes);
-
-module.exports = apiRouter;
