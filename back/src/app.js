eba2803582f74090a328f80ef1d2b80089f045b5
Armando <armandothomazini@gmail.com>
2022-04-24 17:29:25 -0300

    Adicionado configuração CORS

--- back/src/app.js
+++ back/src/app.js
@@ -2,8 +2,10 @@ const express = require('express')
 const app = express()
 const apiRouter = require('../src/routes/index');
 const BDService = require('../src/services/BDService');
+var cors = require('cors')
 app.use(express.json());
 app.use('/', apiRouter);
+app.use(cors())
 innitBD();
 module.exports = app;
 
