30841063af1a66b9e9520caadcc1db8f593afddb
GPancaldi <pancaldi@alunos.utfpr.edu.br>
2022-05-07 10:31:42 -0300

    Fix Teste cadastro professor - arrumando a issue #11

--- back/src/testes/testeCadastroAluno.test.js
+++ back/src/testes/testeCadastroAluno.test.js
@@ -2,14 +2,40 @@ const request = require("supertest");
 const app = require("../app");
 
 
- describe('Teste para checar se o cadastro esta funcionando', function(){
-    it('Teste na rota POST /user', async function(){
+ describe('Testes no cadastro de um aluno', function(){
+    it('Teste na rota POST /user happy way', async function(){
       const response = await request(app)
         .post('/user')
         .send({
             "name" : "Professor",
             "email" : "professor@hotmail.com",
             "password" : "4215",
+            "user_role_id" : 1
+        })
+        expect(200);      
+      
+    })
+
+    it('Teste na rota POST /user badpath - email ja cadastrado', async function(){
+      const response = await request(app)
+        .post('/user')
+        .send({
+            "name" : "Professor teste",
+            "email" : "professor@hotmail.com",
+            "password" : "1234",
+            "user_role_id" : 1
+        })
+        expect(400);      
+      
+    })
+
+    it('Teste na rota POST /user happyway - Outro usuario', async function(){
+      const response = await request(app)
+        .post('/user')
+        .send({
+            "name" : "Professor Girafales",
+            "email" : "professorsalsicha@hotmail.com",
+            "password" : "9876",
             "user_role_id" : 1
         })
         expect(200);      
