c67acd82166f5ef748947c50716ca5c18a493b75
GPancaldi <pancaldi@alunos.utfpr.edu.br>
2022-05-07 11:06:14 -0300

    Fix Testes no login #10

--- back/src/testes/testeLogin.test.js
+++ back/src/testes/testeLogin.test.js
@@ -13,4 +13,27 @@ describe('Teste para checar se o login está funcionando', function(){
         expect(200);      
         
     })
+
+    it('Teste na roda POST /user/login - Login errado', async function(){
+      const response = await request(app)
+        .post('/user/login')
+        .send({
+            "email" : "jonahgr@hotmail.com",
+            "password" : "123"
+        })
+        expect(401);      
+        
+    })
+
+    it('Teste na roda POST /user/login - badpath', async function(){
+      const response = await request(app)
+        .post('/user/login')
+        .send({
+            "email" : "jonahgr@hotmail.com"     
+        })
+        expect(400);      
+        
+    })
+
+  
   });
