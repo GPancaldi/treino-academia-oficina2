b02919010a4f6dbf5c69d9d40912b08ada1895a0
GPancaldi <pancaldi@alunos.utfpr.edu.br>
2022-05-07 10:42:55 -0300

    Fix Teste no Cadastro de aluno #12

--- back/src/testes/testeCadastroAluno.test.js
+++ back/src/testes/testeCadastroAluno.test.js
@@ -2,41 +2,41 @@ const request = require("supertest");
 const app = require("../app");
 
 
- describe('Testes no cadastro de um aluno', function(){
-    it('Teste na rota POST /user happy way', async function(){
+ describe('Teste para checar se o cadastro esta funcionando', function(){
+    it('Teste na rota POST /user', async function(){
       const response = await request(app)
         .post('/user')
         .send({
-            "name" : "Professor",
-            "email" : "professor@hotmail.com",
+            "name" : "Johannes",
+            "email" : "kadada@hotmail.com",
             "password" : "4215",
-            "user_role_id" : 1
+            "user_role_id" : 2
         })
         expect(200);      
       
     })
 
-    it('Teste na rota POST /user badpath - email ja cadastrado', async function(){
+    it('Teste na rota POST /user Badpath - email ja cadastrado', async function(){
       const response = await request(app)
         .post('/user')
         .send({
-            "name" : "Professor teste",
-            "email" : "professor@hotmail.com",
-            "password" : "1234",
-            "user_role_id" : 1
+            "name" : "Jamal",
+            "email" : "kadada@hotmail.com",
+            "password" : "8383",
+            "user_role_id" : 2
         })
         expect(400);      
       
     })
 
-    it('Teste na rota POST /user happyway - Outro usuario', async function(){
+    it('Teste na rota POST /user Cadastro de outro aluno', async function(){
       const response = await request(app)
         .post('/user')
         .send({
-            "name" : "Professor Girafales",
-            "email" : "professorsalsicha@hotmail.com",
-            "password" : "9876",
-            "user_role_id" : 1
+            "name" : "Jamal",
+            "email" : "jamal@email.com",
+            "password" : "8383",
+            "user_role_id" : 2
         })
         expect(200);      
       
--- back/src/testes/testeCadastroProfessor.test.js
+++ back/src/testes/testeCadastroProfessor.test.js
@@ -2,15 +2,41 @@ const request = require("supertest");
 const app = require("../app");
 
 
- describe('Teste para checar se o cadastro esta funcionando', function(){
-    it('Teste na rota POST /user', async function(){
+ describe('Testes no cadastro de um Professor', function(){
+    it('Teste na rota POST /user happy way', async function(){
       const response = await request(app)
         .post('/user')
         .send({
-            "name" : "Johannes",
-            "email" : "kadada@hotmail.com",
+            "name" : "Professor",
+            "email" : "professor@hotmail.com",
             "password" : "4215",
-            "user_role_id" : 2
+            "user_role_id" : 1
+        })
+        expect(200);      
+      
+    })
+
+    it('Teste na rota POST /user badpath - email ja cadastrado', async function(){
+      const response = await request(app)
+        .post('/user')
+        .send({
+            "name" : "Professor teste",
+            "email" : "professor@hotmail.com",
+            "password" : "1234",
+            "user_role_id" : 1
+        })
+        expect(400);      
+      
+    })
+
+    it('Teste na rota POST /user happyway - Outro usuario', async function(){
+      const response = await request(app)
+        .post('/user')
+        .send({
+            "name" : "Professor Girafales",
+            "email" : "professorsalsicha@hotmail.com",
+            "password" : "9876",
+            "user_role_id" : 1
         })
         expect(200);      
       
