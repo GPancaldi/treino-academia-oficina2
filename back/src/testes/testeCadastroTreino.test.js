c142db3229c1a87c978312a5e073f49007c68f7e
GPancaldi <pancaldi@alunos.utfpr.edu.br>
2022-06-02 14:27:31 -0300

    Testes no cadastro de um treino #13

---
+++ back/src/testes/testeCadastroTreino.test.js
@@ -0,0 +1,44 @@
+const request = require("supertest");
+const app = require("../app");
+
+describe('Teste para checar se o cadastro esta funcionando', function(){
+    it('Teste na rota POST /treino', async function(){
+      const response = await request(app)
+        .post('/treino')
+        .send({
+            "name" : "Treino Peito"
+        })
+        expect(200);      
+      
+    })
+
+    it('Teste na rota POST /treino Badpath - treino ja cadastrado', async function(){
+        const response = await request(app)
+          .post('/treino')
+          .send({
+              "name" : "Treino Peito"
+          })
+          expect(401);      
+      })
+
+      it('Teste na rota POST /treino - Cadastrar outro treino', async function(){
+        const response = await request(app)
+          .post('/treino')
+          .send({
+              "name" : "Treino Costas"
+          })
+          expect(200);      
+        
+      })
+
+      it('Teste na rota Post /treino - colocar um campo invalido', async function(){
+        const response = await request(app)
+        .post('/treino')
+        .send({
+            "naime" : "adsasdads"
+        })
+        expect(400);
+
+      })
+
+});
